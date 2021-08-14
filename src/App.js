00ef8fe9be03cd82dc6007d830b2739e7bf4c0ec
Cassie Anderson <cbp.smith@gmail.com>
2021-08-14 13:09:20 -0400

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import ConnectedShoppingList from './components/shopping-list';
 import TheUsual from './components/the-usual';
+import store from './store.js';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { ThemeProvider } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
+import { createTheme } from '@material-ui/core/styles';
 
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store.js';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
 
 const NoMatch = ({ location }) => {
   <div>
@@ -18,28 +39,31 @@ const NoMatch = ({ location }) => {
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <BrowserRouter>
-          <AppBar position="relative">
-              <Toolbar>
-                <Typography variant="h6" color="inherit" noWrap>
-                  Easy Shopper App
-                </Typography>
-              </Toolbar>
-          </AppBar>
-          <div>
-            <ul>
-              <li><Link to="/">Shopping List</Link></li>
-              <li><Link to="/saved">The Usual (Stored List)</Link></li>
-            </ul>
-          </div>
-          <Switch>
-            <Route exact path="/" component={ConnectedShoppingList}/>
-            <Route exact path="/saved" component={TheUsual}/>
-            <Route component={NoMatch}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <BrowserRouter>
+            <AppBar position="relative">
+                <Toolbar>
+                  <Typography variant="h6" color="inherit" noWrap>
+                    Easy Shopper
+                  </Typography>
+                </Toolbar>
+            </AppBar>
+            <div>
+              <ul>
+                <li><Link to="/">Shopping List</Link></li>
+                <li><Link to="/saved">The Usual (Stored List)</Link></li>
+              </ul>
+            </div>
+            <Switch>
+              <Route exact path="/" component={ConnectedShoppingList}/>
+              <Route exact path="/saved" component={TheUsual}/>
+              <Route component={NoMatch}/>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
     </Provider>  
   );
 }
--- src/components/shopping-list.jsx
+++ src/components/shopping-list.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
 class ShoppingList extends (React.Component) {
     render() {
         return (
-            <div>
-                <ul>
-                    {this.props.shopping_list.map((item, index) => {
-                        return <li key = {index}>
-                            <input type="checkbox" />
-                            {item.name}
-                        </li>;
-                    })}
-                </ul>
-            </div>
+            <Card>
+              <CardContent>
+                    <ul>
+                        {this.props.shopping_list.map((item, index) => {
+                            return <li key = {index}>
+                                <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={item.checked}
+                                        // onChange={handleChange}
+                                        color="primary"
+                                    />
+                                    }
+                                    label={item.name}
+                                />
+                            </li>;
+                        })}
+                    </ul>
+              </CardContent>
+              <CardActions>
+                  <Button>Add Item</Button>
+              </CardActions>
+            </Card>
         );
     }
 }
 
+//reads data from state(component) and maps to this.props.shopping_list
 function mapStateToProps(state) {
     return {shopping_list: state.shopping_list}; 
 }
